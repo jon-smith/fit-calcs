47217aefbd90d96f18ed4f800db2352812cd6cfc
jon-smith <26364802+jon-smith@users.noreply.github.com>
2026-08-10 13:20:33 +0100

    App per page/route

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
+import { RouterProvider } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from './App';
+import { router } from './router';
 import './styles.css';
 
 createRoot(document.querySelector('#root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
